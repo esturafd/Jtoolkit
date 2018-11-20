d62dc88ae36c6c3ca77537a9c3dc89f9f7843ae5
Estuardo Ramos <estuardoramos.ec10@gmail.com>
2018-11-20 04:24:10 +0000

    Se agregan mas pruebas para la clase MapProperty

--- src/test/java/test/streameast/toolkit/yml/MapPropertiesTest.java
+++ src/test/java/test/streameast/toolkit/yml/MapPropertiesTest.java
@@ -4,17 +4,34 @@ import static org.junit.Assert.*;
 
 import java.util.Map;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import test.streameast.toolkit.help.ObjectTag;
 
 import com.streameast.toolkit.yml.MapProperty;
 
 public class MapPropertiesTest {
     
+    Map<String, Object> properties;
+    
+    @Before
+    public void initMap() {
+        properties = new MapProperty<String, Object>("properties.yml");
+    }
+    
     @Test
     public void testCargaProperties() {
-        Map<String, String> properties = new MapProperty<String, String>("properties.yml");
-        String version = properties.get("version");
-        System.out.println(properties.toString());
+        String version = properties.get("version").toString();
+        System.out.println("Mapa: " + properties.toString());
         assertEquals("La propiedad no es la esperada " + version, "1.0", version);
+    }
+    
+    @Test
+    public void testCargaPropTags() {
+        ObjectTag object = (ObjectTag) properties.get("object");
+        System.out.println("Object: " + object.toString());
+        assertEquals("No se cargo bien el nombre", object.getNombre(), "Juan");
+        assertEquals("No se cargo bien la edad", object.getEdad(), 27);
     }
 }
