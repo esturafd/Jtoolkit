cdfdc789dc3306b52f673d4966b9d075697a7062
Estuardo Ramos <efrain.ramos.castro@ericsson.com>
2020-05-09 15:10:58 -0600

    Se descontinua la clase Ansi

--- src/main/java/com/esturafd/jtoolkit/console/Ansi.java
+++ src/main/java/com/esturafd/jtoolkit/console/Ansi.java
@@ -2,11 +2,15 @@ package com.esturafd.jtoolkit.console;
 
 /**
  * Help class for ANSI console escape sequences
+ * @deprecated Use {@link com.esturafd.jtoolkit.console.ansi.Attribute Attribute}
+ * o {@link com.esturafd.jtoolkit.console.ansi.Sequence Sequence}
  * 
  * @author esturafd
  */
+@Deprecated
 public class Ansi {
-    
+
+    private Ansi() {}
     public static final String ESC = "\033";
     
     // Cursor control
@@ -53,34 +57,86 @@ public class Ansi {
     public static final String BKG_CYAN = "46";
     public static final String BKG_WHITE = "47";
     
+
+    /**
+     * Formats the attributes to be processed by an ANSI console
+     * @deprecated Use {@link com.esturafd.jtoolkit.console.ansi.Attribute Attribute}
+     * @param attributes 
+     * @return the union of the attributes to be sent to the console
+     */
     public static String displayAttribute(String... attributes) {
         return String.format(DISPLAY, String.join(";", attributes));
     }
     
+    /**
+     * Format text to be colored on console
+     * @deprecated Use {@link com.esturafd.jtoolkit.console.ansi.Text#paint(color, text) paint()}
+     * @param text text to be colored
+     * @param attributes attributes to take into account for coloring
+     * @return the union of the attributes to be sent to the console
+     */
     public static String coloredText(String text, String... attributes) {
         return displayAttribute(attributes) + text + displayAttribute(RESET);
     }
     
+    /**
+     * Move cursor home to {row} and {column}
+     * @deprecated Use {@link com.esturafd.jtoolkit.console.ansi.Cursor#home(int, int) home()}
+     * @param row horizontal coordinate for home
+     * @param column vertical coordinate for home
+     * @return movement attribute to be sent to console
+     */
     public static String cursorHome(int row, int column) {
         return String.format(CRS_HOME, row, column);
     }
     
+    /**
+     * Move cursor to up
+     * @deprecated Use {@link com.esturafd.jtoolkit.console.ansi.Cursor#up(int) up()}
+     * @param count Number of lines to move
+     * @return movement attribute to be sent to console
+     */
     public static String cursorUp(int count) {
         return String.format(CRS_UP, count);
     }
     
+    /**
+     * Move cursor to dowm
+     * @deprecated Use {@link com.esturafd.jtoolkit.console.ansi.Cursor#down(int) down()}
+     * @param count Number of lines to move
+     * @return movement attribute to be sent to console
+     */
     public static String cursorDown(int count) {
         return String.format(CRS_DOWN, count);
     }
     
+    /**
+     * Move cursor to right
+     * @deprecated Use {@link com.esturafd.jtoolkit.console.ansi.Cursor#forward(int) forward()}
+     * @param count Number of spaces to move
+     * @return movement attribute to be sent to console
+     */
     public static String cursorForward(int count) {
         return String.format(CRS_FORWARD, count);
     }
     
+    /**
+     * Move cursor to left
+     * @deprecated Use {@link com.esturafd.jtoolkit.console.ansi.Cursor#backward(int) backward()}
+     * @param count Number of spaces to move
+     * @return movement attribute to be sent to console
+     */
     public static String cursorBackward(int count) {
         return String.format(CRS_BACKWARD, count);
     }
     
+    /**
+     * Move cursor to position
+     * @deprecated Use {@link com.esturafd.jtoolkit.console.ansi.Cursor#home(int, int) home()}
+     * @param row Horizontal position
+     * @param column Vertical position
+     * @return movement attribute to be sent to console
+     */
     public static String cursorPosition(int row, int column) {
         return String.format(CRS_FPOSITION, row, column);
     }
